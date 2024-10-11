dd75a5d192f544e2a80c523bb8245bf0aad4b058
joey <t1616joy@yahoo.com.tw>
2024-10-11 21:11:07 +0800

    tree 94 solution 1

---
+++ tree/easy/tree_94.py
@@ -0,0 +1,41 @@
+from typing import Optional, List
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        stack = []
+        current = root
+        result = []
+        while current or stack:
+            while current:
+                stack.append(current)
+                current = current.left
+            current = stack.pop()
+            result.append(current.val)
+            current = current.right
+        return result
+            
+
+# root = [1,None,2,3]
+
+# 建立二元樹節點
+root = [1,2,3,4,5,None,8,None,None,6,7,9]
+root = TreeNode(1)
+root.left = TreeNode(2)
+root.right = TreeNode(3)
+root.left.left = TreeNode(4)
+root.left.right = TreeNode(5)
+root.left.right.left = TreeNode(6)
+root.left.right.right = TreeNode(7)
+root.right.right = TreeNode(8)
+root.right.right.left = TreeNode(9)
+
+
+# 創建解法實例並調用中序遍歷方法
+solution = Solution()
+output = solution.inorderTraversal(root)
+print(output)  # 輸出：[4, 2, 6, 5, 9, 7, 1, 3, 8]
