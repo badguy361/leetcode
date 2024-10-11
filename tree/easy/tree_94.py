d226b731749fb8886ad7a8f82e610683c717a998
joey <t1616joy@yahoo.com.tw>
2024-10-11 21:11:26 +0800

    tree 94 solution 1

--- tree/easy/tree_94.py
+++ tree/easy/tree_94.py
@@ -1,3 +1,41 @@
+"""
+94. Binary Tree Inorder Traversal
+
+Given the root of a binary tree, return the inorder traversal of its nodes' values.
+
+
+Example 1:
+
+Input: root = [1,null,2,3]
+
+Output: [1,3,2]
+
+Example 2:
+
+Input: root = [1,2,3,4,5,null,8,null,null,6,7,9]
+
+Output: [4,2,6,5,7,1,3,9,8]
+
+Example 3:
+
+Input: root = []
+
+Output: []
+
+Example 4:
+
+Input: root = [1]
+
+Output: [1]
+"""
+
+# solution:
+"""
+做法: 中序遍歷(左中右)
+1. 使用stack來儲存節點
+2. 使用current來遍歷節點
+3. 使用result來儲存結果
+"""
 from typing import Optional, List
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
