39a1f1b5581379e8d81ca9438250f0af4b51d81a
joey <t1616joy@yahoo.com.tw>
2024-10-17 22:50:03 +0800

    tree 108 solution

--- tree/easy/tree_108.py
+++ tree/easy/tree_108.py
@@ -5,27 +5,30 @@ order, convert it to a height-balanced binary search tree.
 A height-balanced binary tree is a binary tree in which the depth of 
 the two subtrees of every node never differs by more than one.
 """
+
+# solution 1
 from typing import Optional, List
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
         self.left = left
         self.right = right
+
 class Solution:
     def sortedArrayToBST(self, nums: List[int]) -> Optional[TreeNode]:
-        def rec(nums: List[int]) -> Optional[TreeNode]:
-            if len(nums) == 0:
-                return None
-            root_num = len(nums)//2
-            print("root_num:", root_num)
-            root = TreeNode(nums[root_num])
-            print(root.val)
-            print("---")
-            root.left = rec(nums[:root_num])
-            root.right = rec(nums[root_num+1:])
-            return root
-        return rec(nums)
+        total_nums = len(nums)
+        print("total_nums:", nums)
+        if not total_nums:
+            return None
 
-nums = [-10,-3,0,5,9]
+        mid_node = total_nums // 2
+        return TreeNode(
+            nums[mid_node],
+            self.sortedArrayToBST(nums[:mid_node]), self.sortedArrayToBST(nums[mid_node + 1 :])
+        )
+# 示例输入
+nums = [-10, -3, 0, 5, 9]
 root = Solution().sortedArrayToBST(nums)
 print(root.val)
+print(root.left.val)
+print(root.right.val)
