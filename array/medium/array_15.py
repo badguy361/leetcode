07f20832a816276479da2f8a64eccec5fd6ab36d
joey <t1616joy@yahoo.com.tw>
2025-08-06 22:32:10 +0800

    update array 15 solution 2

--- array/medium/array_15.py
+++ array/medium/array_15.py
@@ -49,7 +49,7 @@ nums = [3,0,-2,-1,1,2]
 複雜度: O(n^3)
 """
 nums.sort()
-ans =  set()
+# ans =  set()
 # for i in range(len(nums)-1):
 #     k = len(nums)-1
 #     while k > i:
@@ -66,25 +66,25 @@ ans =  set()
 3. 用set去重
 複雜度: O(n^2)
 """
-for i in range(len(nums) - 2):
+ans=[]
+for i,num in enumerate(nums):
     if i > 0 and nums[i] == nums[i - 1]:
-        continue  # 跳過重複元素
-    left, right = i + 1, len(nums) - 1
-    while left < right:
-        total = nums[i] + nums[left] + nums[right]
-        if total == 0:
-            ans.add((nums[i], nums[left], nums[right]))
-            while left < right and nums[left] == nums[left + 1]:
-                left += 1  # 跳過重複元素
-            while left < right and nums[right] == nums[right - 1]:
-                right -= 1  # 跳過重複元素
-            left += 1
-            right -= 1
-        elif total < 0:
-            left += 1
+        continue
+    left = i+1
+    right = len(nums)-1
+    while(right>left):
+        total = num+nums[left]+nums[right]
+        if total>0:
+            right-=1
+        elif total<0:
+            left+=1
         else:
-            right -= 1
+            ans.append([num,nums[left],nums[right]])
+            while(right>left and nums[right]==nums[right-1]):
+                right-=1
+            while(right>left and nums[left]==nums[left+1]):
+                left+=1
+            right-=1
+            left+=1
 
-ans_list = [list(t) for t in ans]
-ans_list.sort()
-print(ans_list)
+print(ans)
