af2aa332cac6edc5f7cbb148729872f55f29a9a1
joey <t1616joy@yahoo.com.tw>
2025-09-25 22:18:45 +0800

    question(binary search 704): Recursive

--- binary_search/easy/binary_search_704.py
+++ binary_search/easy/binary_search_704.py
@@ -16,7 +16,7 @@ nums is sorted in ascending order.
 """
 # solution 1
 """
-做法: binary search
+做法: binary search 迴圈
 複雜度:
 - 時間複雜度: O(logn)
 - 空間複雜度: O(1)
@@ -35,3 +35,28 @@ while left <= right:
         right = mid - 1
 
 print(-1)
+
+# solution 2
+"""
+做法: binary search 遞迴
+複雜度:
+- 時間複雜度: O(logn)
+- 空間複雜度: O(logn)
+    每次呼叫函式都要壓一層 stack frame，所以會額外用到遞迴深度的記憶體
+"""
+def search(nums: List[int], target: int) -> int:
+    def binarySearch(left: int, right: int) -> int:
+        if left > right:
+            return -1
+        
+        mid = (left + right) // 2
+        if nums[mid] == target:
+            return mid
+        elif nums[mid] < target:
+            return binarySearch(mid + 1, right)
+        else:
+            return binarySearch(left, mid - 1)
+    
+    return binarySearch(0, len(nums) - 1)
+
+print(search(nums, target))
