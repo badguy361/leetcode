fc13be2cb0d580722b77744cbe81e4daf95cdd4f
joey <t1616joy@yahoo.com.tw>
2025-10-08 22:32:53 +0800

    question(binary search 981)

---
+++ binary_search/medium/binary_search_981.py
@@ -0,0 +1,68 @@
+"""
+981. Time Based Key-Value Store
+
+Design a time-based key-value data structure that can store multiple values for the same key
+at different time stamps and retrieve the key's value at a certain timestamp.
+
+Implement the TimeMap class:
+
+- TimeMap() Initializes the object of the data structure.
+void set(String key, String value, int timestamp) Stores the key 
+key with the value value at the given time timestamp.
+- String get(String key, int timestamp) Returns a value such that set 
+was called previously, such that timestamp_prev <= timestamp. If there 
+are multiple such values, it returns the value associated with the largest 
+timestamp_prev. If there are no values, it returns "".
+
+Example 1:
+
+Input
+["TimeMap", "set", "get", "get", "set", "get", "get"]
+[[], ["foo", "bar", 1], ["foo", 1], ["foo", 3], ["foo", "bar2", 4], ["foo", 4], ["foo", 5]]
+Output
+[null, null, "bar", "bar", null, "bar2", "bar2"]
+
+Explanation
+TimeMap timeMap = new TimeMap();
+timeMap.set("foo", "bar", 1);  // store the key "foo" and value "bar" along with timestamp = 1.
+timeMap.get("foo", 1);         // return "bar"
+timeMap.get("foo", 3);         // return "bar", since there is no 
+                               // value corresponding to foo at timestamp 3 and timestamp 2, 
+                               // then the only value is at timestamp 1 is "bar".
+timeMap.set("foo", "bar2", 4); // store the key "foo" and value "bar2" along with timestamp = 4.
+timeMap.get("foo", 4);         // return "bar2"
+timeMap.get("foo", 5);
+
+"""
+
+# solution 1
+"""
+做法: binary search
+複雜度:
+- 時間複雜度: O(logn)
+- 空間複雜度: O(n)
+"""
+class TimeMap:
+
+    def __init__(self):
+        self.store = {}
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        if key not in self.store:
+            self.store[key] = []
+        self.store[key].append([value,timestamp])
+
+    def get(self, key: str, timestamp: int) -> str:
+        total = self.store.get(key)
+        if not total:
+            return ""
+        left, right = 0, len(total)-1
+        ans = ""
+        while left <= right:
+            mid = (left + right) // 2
+            if total[mid][1] <= timestamp:
+                ans = total[mid][0]
+                left = mid + 1
+            else:
+                right = mid - 1
+        return ans
