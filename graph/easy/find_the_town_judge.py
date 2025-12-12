ff0e52db6abd1abd53c29ecd5b79860557215312
joey <t1616joy@yahoo.com.tw>
2025-12-12 09:56:26 +0800

    question(find_the_town_judge): use adjacency list

--- graph/easy/find_the_town_judge.py
+++ graph/easy/find_the_town_judge.py
@@ -2,8 +2,15 @@
 997. Find the Town Judge
 """
 # solution 1:
+"""
+作法:
+計算 indegree & outdegree list
+時間複雜度: O(n)
+空間複雜度: O(n)
+"""
 from typing import List
-class Solution:
+from collections import defaultdict
+class Solution1:
     def findJudge(self, n: int, trust: List[List[int]]) -> int:
         if n == 1:
             return 1
@@ -16,5 +23,24 @@ class Solution:
 
         for person in range(n+1):
             if indegree[person] == n-1 and outdegree[person] == 0:
+                return person
+        return -1
+
+# solution 2:
+"""
+作法:
+使用Adjacency list
+時間複雜度: O(n)
+空間複雜度: O(n)
+"""
+class Solution2:
+    def findJudge(self, n: int, trust: List[List[int]]) -> int:
+        graph = defaultdict(list)
+        indegree = [0]*(n+1)
+        for a, b in trust:
+            graph[a].append(b)
+            indegree[b] += 1
+        for person in range(1, n+1):
+            if len(graph[person])==0 and indegree[person] == n-1:
                 return person
         return -1
